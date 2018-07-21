151ae19ad669bb56c8670b93af9017f60c86bf57
bstorozhuk <storozhuk.b.m@gmail.com>
2018-07-22 00:27:56 +0300

    Coverage statistic. Documentation update.

--- verifier.go
+++ verifier.go
@@ -13,7 +13,7 @@ var verificationsWriter = unsafe.Pointer(&os.Stdout)
 
 // New creates verification instance (recommended).
 // It tracks verification state.
-// If you forget to check internal error, using Verify.GetError or Verify.PanicOnError methods,
+// If you forget to check internal error, using `GetError` or `PanicOnError` methods,
 // it will write error message to UnhandledVerificationsWriter (default: os.Stdout).
 // This mechanism will help you track down possible unhandled verifications.
 // If you don't wan't to track anything, create verifier with Silent() function.
@@ -37,7 +37,7 @@ func Silent() *Verify {
 
 // Offensive creates verification instance (not-recommended).
 // It tracks verification state and stops application process when founds unchecked verification.
-// If you forget to check internal error, using Verify.GetError or Verify.PanicOnError methods,
+// If you forget to check internal error, using `GetError` or `PanicOnError` methods,
 // it will write error message to UnhandledVerificationsWriter (default: os.Stdout) and WILL STOP YOUR PROCESS.
 // Created for people who adopt offensive programming(https://en.wikipedia.org/wiki/Offensive_programming).
 // This mechanism will help you track down possible unhandled verifications.
@@ -52,7 +52,7 @@ func Offensive() *Verify {
 }
 
 // Verify represents verification instance.
-// All checks can be performed on it using Verify.That or Verify.Predicate functions.
+// All checks can be performed on it using `That` or `Predicate` functions.
 // After one failed check all others won't count and predicates won't be evaluated.
 // Use Verify.GetError function to check if there where any during verification process.
 type Verify struct {
@@ -62,8 +62,8 @@ type Verify struct {
 }
 
 // That verifies condition passed as first argument.
-// If positiveCondition == true, verification will proceed for other checks.
-// If positiveCondition == false, internal state will be filled with error,
+// If `positiveCondition == true`, verification will proceed for other checks.
+// If `positiveCondition == false`, internal state will be filled with error,
 // using message argument as format in fmt.Errorf(message, args...).
 // After the first failed verification all others won't count and predicates won't be evaluated.
 func (v *Verify) That(positiveCondition bool, message string, args ...interface{}) {
@@ -78,8 +78,8 @@ func (v *Verify) That(positiveCondition bool, message string, args ...interface{
 }
 
 // That evaluates predicate passed as first argument.
-// If predicate() == true, verification will proceed for other checks.
-// If predicate() == false, internal state will be filled with error,
+// If `predicate() == true`, verification will proceed for other checks.
+// If `predicate() == false`, internal state will be filled with error,
 // using message argument as format in fmt.Errorf(message, args...).
 // After the first failed verification all others won't count and predicates won't be evaluated.
 func (v *Verify) Predicate(predicate func() bool, message string, args ...interface{}) {
